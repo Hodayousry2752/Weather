c82547e043784e224da2052bdbf7ff70c6b326ff
Hodayousry2752 <75079203+Hodayousry2752@users.noreply.github.com>
2023-10-09 00:16:51 +0300

    Add files via upload

---
+++ js/index-js.js
@@ -0,0 +1,19 @@
+
+
+"use strict"
+
+// let thisDay=document.getElementById("today");
+// let dateOfDay=document.getElementById("dateOfDay");
+
+
+// async function apiFun() {
+//     var weatherUrl= await fetch('http://api.weatherapi.com/v1/current.json?key=4e46102144114ba1abe142504220606&q=egypt&aqi=yes');
+//     var apiArr=await weatherUrl.json();
+//     console.log(apiArr);
+//     display();
+// }
+// apiFun();
+
+// function display(){
+//     thisDay.innerHTML=apiArr.
+// }
